6178e686ad8df59c71754eb9b76a0b924d5afcb9
lugamecooper <legamerfdp81@gmail.com>
2024-09-03 22:59:44 +0200

    .

--- serveur/restart.py
+++ serveur/restart.py
@@ -3,4 +3,5 @@ from time import sleep
 from os import startfile
 def restart() -> None:
     sleep(10)
-    startfile(f"python",arguments=f"{join(split(__file__)[0],"main.py")}")
+    test = f"{join(split(__file__)[0],'main.py')}"
+    startfile("python",arguments=test)
